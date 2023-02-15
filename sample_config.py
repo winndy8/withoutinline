2eb522103313316fdcbd53b326ee8e8e849e3dc6
winndy8 <125177960+winndy8@users.noreply.github.com>
2023-02-15 13:29:52 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -23,7 +23,7 @@ class Config(object):
     # Authorized users to use this bot
 
     # Generate a user session string
-    TG_USER_SESSION = os.environ.get("TG_USER_SESSION", "")
+    TG_USER_SESSION = os.environ.get("TG_USER_SESSION", "BQGDMIkAvfdYWc-mTHLyC5V0RJ597VWB5VJnYjPlTncPPhn4VkoQAiZaBbBVJkFzLm84o5p9MORq5KH8X7I__yeC8F06DUkv33QgNNyok_MSOSolX0pcoo1chYnoUEH6HoNutchkh07UmzOejZ9B3wJEe5OS7zKtkdC3gGyFEJCnZaPsd-qV9K2mLirPMv9O49PMraivxTH4D2nyzfUwRF9bCukRAMo0QYuHf6ZBrVnMKHOf7RAQ0DcdxdxTHmJJfsPMF0rJIwvzx62j6xiGrPT96O_f3uRZe_lQJEHadKNMAxAvFHPm7tGbF8YolMzMf8J6DXkq4_KcWLYgLFlUesyb6PlNbwAAAAFUPt0PAA")
 
 
 def LOGGER(name: str) -> logging.Logger:
